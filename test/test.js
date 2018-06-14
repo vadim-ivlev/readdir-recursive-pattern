c3cdcc599b040dbf48ede7acf8c7e3c735571053
vadim-ivlev <vadim.ivlev@gmail.com>
2018-06-14 16:37:18 +0300

    added ava test runner

--- test/test.js
+++ test/test.js
@@ -1,37 +1,40 @@
 // jshint esversion:6
-const fs     = require('fs');
-const path   = require('path');
-const assert = require('assert');
+// const fs        = require('fs');
+// const path      = require('path');
+// const test      = require('ava');
+// const listFiles = require('../');
 
-const listFiles = require('../');
-
-let all=listFiles('testdir');
-let html=listFiles('testdir', /\.html$/ );
-
-// console.log(all);
-// console.log();
-// console.log( html );
+import fs from 'fs';
+import path from 'path';
+import test from 'ava';
+import listFiles from '../';
 
 
 
-assert.deepEqual(
-    all.sort(), 
-    [ 'testdir/a.html',
-    'testdir/b.html',
-    'testdir/c.html',
-    'testdir/sub0/1.gif',
-    'testdir/sub0/2.gif',
-    'testdir/sub0/3.jpg',
-    'testdir/sub0/sub1/1.htm',
-    'testdir/sub0/sub1/2.htm' ].sort()
-);
-
-assert.deepEqual(
-    html.sort(), 
-    ["testdir/a.html", "testdir/b.html", "testdir/c.html"].sort()
-);
-
-console.log('passed.');
 
 
 
+test('list all files', t => {
+	t.deepEqual(
+        listFiles('testdir', /^((?!DS_Store).)*$/ ).sort(), // negative lookahead
+        [ 'testdir/a.html',
+        'testdir/b.html',
+        'testdir/c.html',
+        'testdir/sub0/1.gif',
+        'testdir/sub0/2.gif',
+        'testdir/sub0/3.jpg',
+        'testdir/sub0/sub1/1.htm',
+        'testdir/sub0/sub1/2.htm' ].sort()
+    );
+});
+
+test('list HTML files',  t => {
+    t.deepEqual(
+        listFiles('testdir', /\.html$/ ).sort(), 
+        ["testdir/a.html", "testdir/b.html", "testdir/c.html"].sort()
+    );
+});
+
+test('just a number', t=>{
+    t.deepEqual(1,1);
+});
